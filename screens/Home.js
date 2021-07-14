40b0e985b3e3f980e57543cc2af06f3c521354ee
18521487@gm.uit.edu.vn <18521487@gm.uit.edu.vn>
2021-07-14 10:26:06 +0700

    Home(renderContinueWatchingSection) and MovieDetails(finished UI)

--- screens/Home.js
+++ screens/Home.js
@@ -12,7 +12,7 @@ import {
 	ScrollView,
 	ColorPropType
 } from 'react-native';
-import { Profiles } from '../components';
+import { Profiles, ProgressBar } from '../components';
 import { dummyData, COLORS, SIZES, FONTS, icons, images } from '../constants';
 import { newSeason } from '../constants/dummy';
 
@@ -231,6 +231,90 @@ const Home = ({ navigation }) => {
 		)
 	}
 
+	const renderContinueWatchingSection = () => {
+		return (
+			<View
+				style={{
+					marginTop: SIZES.padding
+				}}
+			>
+				{/* Headers */}
+				<View
+					style={{
+						flexDirection: 'row',
+						paddingHorizontal: SIZES.padding,
+						alignItems: 'center'
+					}}
+				>
+					<Text style={{flex: 1, color: COLORS.white, ...FONTS.h2
+					}}>Continue Watching</Text>
+
+					<Image
+						source={icons.right_arrow}
+						style={{
+							width: 20,
+							height: 20,
+							tintColor: COLORS.primary
+						}}
+					/>
+				</View>
+
+
+				{/* List */}
+				<FlatList
+					horizontal
+					showsHorizontalScrollIndicator={false}
+					contentContainerStyle={{
+						marginTop: SIZES.padding
+					}}
+					data={dummyData.continueWatching}
+					keyExtractor={item => `${item.id}`}
+					renderItem={({ item, index }) => {
+						return (
+							<TouchableWithoutFeedback
+								onPress={() => navigation.navigate("MovieDetail", {
+									selectedMovie: item
+								})}
+							>
+								<View
+									style={{
+										marginLeft: index == 0 ? SIZES.padding : 20,
+										marginRight: index == dummyData.continueWatching.length - 1 ? SIZES.padding : 0
+									}}
+								>
+									{/* Thumbnail */}
+									<Image
+										source={item.thumbnail}
+										resizeMode='cover'
+										style={{
+											width: SIZES.width / 3,
+											height: (SIZES.width / 3) + 60,
+											borderRadius: 20
+										}}
+									/>
+
+									{/* Name */}
+									<Text style={{ marginTop: SIZES.base, color: COLORS.white, ...FONTS.h4 }}>{item.name}</Text>
+
+									{/* ProcessBar */}
+									<ProgressBar 
+										containerStyle={{
+											marginTop: SIZES.radius,
+										}}
+										barStyle={{
+											height: 3
+										}}
+										barPercentage={item.overallProgress}
+									/>
+								</View>
+							</TouchableWithoutFeedback>
+						)
+					}}
+				/>
+			</View>
+		)
+	}
+
 	return (
 		<SafeAreaView
 			style={{
@@ -247,6 +331,7 @@ const Home = ({ navigation }) => {
 			>
 				{renderNewSeasonSection()}
 				{renderDots()}
+				{renderContinueWatchingSection()}
 			</ScrollView>
 		</SafeAreaView>
 	)
